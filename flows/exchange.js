e89a994c582e2e21949e76ce06de60ce696e5c9e
yohanderjose2002 <yohanderjose2002@gmail.com>
2025-09-26 01:40:39 +0000

    up

--- flows/exchange.js
+++ flows/exchange.js
@@ -143,6 +143,7 @@ const exchangeFlow = {
 };
 
 function showConfirmation(ctx) {
+    const tasa = await getBTC();
     const amountToReceive = ctx.session.amount;
     const totalInUSD = amountToReceive + COMISION_USD;
     const totalInBolivares = totalInUSD * TASA_BOLIVAR;
